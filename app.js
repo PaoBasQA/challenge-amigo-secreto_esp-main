1292ce3d7d4e894631a7fbb31c8aabc085406aa5
PaoBas <paobas113@gmail.com>
2025-02-03 16:26:25 -0300

    Función para asignar texto a un elemento HTML

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
 
 //Inicia declarando una variable de tipo array, que almacenará los nombres de los amigos ingresados
 let amigos = [];
+
+//Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector 
+// para obtener el texto ingresado por el usuario.
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+
 
