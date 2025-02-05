ec868cd285a06f452fad47604653fbdcbdbf643e
PaoBas <paobas113@gmail.com>
2025-02-05 20:01:52 -0300

    mientras voy repasando, voy armando el prg.

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
-//Inicia declarando una variable de tipo array, que almacenará los nombres de los amigos ingresados
+// Declarar el array que almacenará los nombres de los amigos
 let amigos = [];
 
-//Capturar el valor del campo de entrada: Utilizar document.getElementById o document.querySelector 
-// para obtener el texto ingresado por el usuario.
+// Función para agregar amigos
+    function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();  //Elimina espacios en blanco
+}
 
-function asignarTextoElemento(elemento, texto) {
+/*function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return; //no hace nada, solo finaliza la función.
-}
+}*/
 
 //Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. 
 // Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
-
 if (amigos === "") {
     alert("Por favor, inserte un nombre.");
     return;
 }
+
+//Actualizar el array de amigos: Si el valor es válido, 
+//añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
+amigos.push(nombre);
+
